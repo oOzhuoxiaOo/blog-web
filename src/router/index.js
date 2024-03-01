ac00b80eebb7b6b0c83b1f0e2accc70033bcd897
oOzhuoxiaOo <1851428933@qq.com>
2024-03-01 14:32:00 +0800

    添加谷歌网站分析代码块

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 import { createRouter, createWebHashHistory } from "vue-router"
 // 路由懒加载(按需加载)
 const Home = () => import("@/pages/Home.vue");
-const UserLogin = () => import("@/pages/user/UserLogin.vue");
-const UserRegister = () => import("@/pages/user//UserRegister.vue");
-const UserResetPassword = () => import("@/pages/user/UserResetPassword.vue");
 const BlogNote = () => import("@/pages/BlogNote.vue");
-const Admin = () => import("@/pages/admin/Admin.vue");
+
 const NoteContent = () => import("@/pages/note/NoteContent.vue");
 const NoteNav = () => import("@/pages/note/NoteNav.vue");
 const NoteCate = () => import("@/pages/note/NoteCate.vue");
