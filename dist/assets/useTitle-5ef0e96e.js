6866f2c1fc42bd7a5e7a20b0fec63b54a7406c4d
oOzhuoxiaOo <1851428933@qq.com>
2024-03-01 14:40:20 +0800

    添加谷歌分析工具

---
+++ dist/assets/useTitle-5ef0e96e.js
@@ -0,0 +1,1 @@
+import{z as o,l as s}from"./index-0449f5a3.js";import{u as r}from"./index-1298a90b.js";const i=()=>{const e=o(),t=r();s(()=>{t.meta.hasOwnProperty("title")&&(e.title=t.meta.title)})};export{i as u};
